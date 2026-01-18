81fed552ba5ecd8c71e94ab62649532eb70bb273
akghosh111 <ghoshkiran2017@gmail.com>
2026-01-18 22:43:31 +0530

    minor fixes & changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,7 +24,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required!")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -33,7 +33,7 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required");
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,9 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         // file has been uploaded successfully
-        console.log("File uploaded on Cloudinary successfully", response.url);
+        // console.log("File uploaded on Cloudinary successfully", response.url);
+
+        fs.unlinkSync(localFilePath)
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
